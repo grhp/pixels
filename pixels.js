0aef337f8048a7cada4cfc563c769788578dfba5
Philip Rideout <philiprideout@gmail.com>
2018-09-11 19:18:55 -0700

    n-2 seems to do the trick

--- pixels.js
+++ pixels.js
@@ -49,13 +49,22 @@ function draw() {
     
     ctx.stroke();
 
-    // For Android Chrome, looks like we need to do "n - 1.5" for right-most / bottom-most. (!)
+    // For Android Chrome, looks like we need to do n-3 for right-most / bottom-most.
+    // The black lines above (n-1) do not show up, not does the red one below (n-2). However the
+    // green line (n-3) seems to be line up exactly in the upper row.
     ctx.strokeStyle = '#a00';
     ctx.beginPath();
-    ctx.moveTo(150, fbheight - 1.5);
-    ctx.lineTo(250, fbheight - 1.5);
-    ctx.moveTo(fbwidth - 1.5, 150);
-    ctx.lineTo(fbwidth - 1.5, 250);
+    ctx.moveTo(100, fbheight - 1.5);
+    ctx.lineTo(200, fbheight - 1.5);
+    ctx.moveTo(fbwidth - 1.5, 100);
+    ctx.lineTo(fbwidth - 1.5, 200);
+    ctx.stroke();
+    ctx.strokeStyle = '#0a0';
+    ctx.beginPath();
+    ctx.moveTo(100, fbheight - 2.5);
+    ctx.lineTo(200, fbheight - 2.5);
+    ctx.moveTo(fbwidth - 2.5, 100);
+    ctx.lineTo(fbwidth - 2.5, 200);
     ctx.stroke();
 
     window.requestAnimationFrame(draw);
