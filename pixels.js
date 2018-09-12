2fc8d14c45423199fd646ff272a5a6c6d27757a6
Philip Rideout <philiprideout@gmail.com>
2018-09-11 19:02:30 -0700

    Try n-1 for Android Chrome.

--- pixels.js
+++ pixels.js
@@ -27,24 +27,36 @@ function window_resize() {
 function draw() {
     var ctx = document.getElementById('mycanvas').getContext('2d');
     ctx.clearRect(0, 0, fbwidth, fbheight);
+
+    ctx.strokeStyle = '#000';
     ctx.beginPath();
 
-    // Three horizontal lines
-    ctx.moveTo(100, 100.5);
-    ctx.lineTo(200, 100.5);
+    // Three horizontal lines: left-most column, 100th column, and right-most column.
     ctx.moveTo(100, 0.5);
     ctx.lineTo(200, 0.5);
+    ctx.moveTo(100, 100.5);
+    ctx.lineTo(200, 100.5);
     ctx.moveTo(100, fbheight - 0.5);
     ctx.lineTo(200, fbheight - 0.5);
 
-    // Three vertical lines
-    ctx.moveTo(100.5, 100);
-    ctx.lineTo(100.5, 200);
+    // Three vertical lines: top-most row, 100th row, and bottom-most row.
     ctx.moveTo(0.5, 100);
     ctx.lineTo(0.5, 200);
+    ctx.moveTo(100.5, 100);
+    ctx.lineTo(100.5, 200);
     ctx.moveTo(fbwidth - 0.5, 100);
     ctx.lineTo(fbwidth - 0.5, 200);
     
     ctx.stroke();
+
+    // For Android Chrome, looks like we need to do "n - 1.5" for right-most / bottom-most. (!)
+    ctx.strokeStyle = '#a00';
+    ctx.beginPath();
+    ctx.moveTo(150, fbheight - 1.5);
+    ctx.lineTo(250, fbheight - 1.5);
+    ctx.moveTo(fbwidth - 1.5, 150);
+    ctx.lineTo(fbwidth - 1.5, 250);
+    ctx.stroke();
+
     window.requestAnimationFrame(draw);
 }
