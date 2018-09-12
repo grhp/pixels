12883a081c1fe9c1cbc40a75f5dfb3148f2a3f27
Philip Rideout <philiprideout@gmail.com>
2018-09-11 19:40:07 -0700

    Add README and use translate().

--- pixels.js
+++ pixels.js
@@ -6,6 +6,7 @@ window.onload = function() {
     let ctx = canvas.getContext('2d');
     ctx.lineWidth = 1;
     ctx.globalCompositeOperation = 'destination-over';
+
     window_resize();
     window.addEventListener("resize", window_resize);
     window.requestAnimationFrame(draw);
@@ -20,8 +21,16 @@ function window_resize() {
     canvas.height = css_height * dpr;
     console.info(css_width + 'x' + css_height + '@' + dpr);
 
-    fbwidth = canvas.width;
-    fbheight = canvas.height;
+    // Note that we're converting this into an integer; on some devices, the pixel ratio * virtual
+    // pixels does NOT result in integer dimensions, but it's the best approximation we have to
+    // physical devices.
+    fbwidth = canvas.width | 0;
+    fbheight = canvas.height | 0;
+
+    // This is required to draw single-pixel lines using integer coordinates.
+    let ctx = canvas.getContext('2d');
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.translate(0.5, 0.5);
 }
 
 function draw() {
@@ -31,40 +40,38 @@ function draw() {
     ctx.strokeStyle = '#000';
     ctx.beginPath();
 
-    // Three horizontal lines: left-most column, 100th column, and right-most column.
-    ctx.moveTo(100, 0.5);
-    ctx.lineTo(200, 0.5);
-    ctx.moveTo(100, 100.5);
-    ctx.lineTo(200, 100.5);
-    ctx.moveTo(100, fbheight - 0.5);
-    ctx.lineTo(200, fbheight - 0.5);
+    // Three horizontal lines: left-most column, 99th column, and right-most column.
+    ctx.moveTo(100, 0);
+    ctx.lineTo(200, 0);
+    ctx.moveTo(100, 100);
+    ctx.lineTo(200, 100);
+    ctx.moveTo(100, fbheight - 1);
+    ctx.lineTo(200, fbheight - 1);
 
-    // Three vertical lines: top-most row, 100th row, and bottom-most row.
-    ctx.moveTo(0.5, 100);
-    ctx.lineTo(0.5, 200);
-    ctx.moveTo(100.5, 100);
-    ctx.lineTo(100.5, 200);
-    ctx.moveTo(fbwidth - 0.5, 100);
-    ctx.lineTo(fbwidth - 0.5, 200);
+    // Three vertical lines: top-most row, 99th row, and bottom-most row.
+    ctx.moveTo(0, 100);
+    ctx.lineTo(0, 200);
+    ctx.moveTo(100, 100);
+    ctx.lineTo(100, 200);
+    ctx.moveTo(fbwidth - 1, 100);
+    ctx.lineTo(fbwidth - 1, 200);
     
     ctx.stroke();
 
-    // For Android Chrome, looks like we need to do n-3 for right-most / bottom-most.
-    // The black lines above (n-1) do not show up, not does the red one below (n-2). However the
-    // green line (n-3) seems to be line up exactly in the upper row.
+    // Draw a red lines at (n-2) and a green line at (n-3).
     ctx.strokeStyle = '#a00';
     ctx.beginPath();
-    ctx.moveTo(100, fbheight - 1.5);
-    ctx.lineTo(200, fbheight - 1.5);
-    ctx.moveTo(fbwidth - 1.5, 100);
-    ctx.lineTo(fbwidth - 1.5, 200);
+    ctx.moveTo(90, fbheight - 2);
+    ctx.lineTo(210, fbheight - 2);
+    ctx.moveTo(fbwidth - 2, 90);
+    ctx.lineTo(fbwidth - 2, 210);
     ctx.stroke();
     ctx.strokeStyle = '#0a0';
     ctx.beginPath();
-    ctx.moveTo(100, fbheight - 2.5);
-    ctx.lineTo(200, fbheight - 2.5);
-    ctx.moveTo(fbwidth - 2.5, 100);
-    ctx.lineTo(fbwidth - 2.5, 200);
+    ctx.moveTo(80, fbheight - 3);
+    ctx.lineTo(220, fbheight - 3);
+    ctx.moveTo(fbwidth - 3, 80);
+    ctx.lineTo(fbwidth - 3, 220);
     ctx.stroke();
 
     window.requestAnimationFrame(draw);
